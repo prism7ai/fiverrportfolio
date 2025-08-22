0559c6bbbd795c24082cc459f10e17a2d3f7438b
Priyaranjan R <71543802+prism7ai@users.noreply.github.com>
2025-08-22 20:50:30 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <a href="#portfolio">Portfolio</a>
+       
 
         <nav>
           <ul>
